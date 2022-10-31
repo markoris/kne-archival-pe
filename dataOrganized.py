822b412dc77bc0872b63879f35a85af90b012da7
moncluse <moncluse24@gmail.com>
2022-10-30 20:45:11 -0400

    cleaned up print statements

--- dataOrganized.py
+++ dataOrganized.py
@@ -1,15 +1,14 @@
+#working on cleaning up print statements
 import glob
 import json
 filepath = glob.glob("/home/emily.monclus/ArchiveData/kne-2000-2029/*")
 filepath2 = glob.glob("/home/emily.monclus/ArchiveData/kne-internal/*")
 
-def fxnname(filepath):
+def systemCounts(filepath):
 	for file in filepath:
 		print(file)
 		list = file.split("/")
-		print(list)
 		last = list[-1]
-		print(f" Last: {last}")
 		if last == "LICENSE":
 			print("end")
 		else:
@@ -17,33 +16,27 @@ def fxnname(filepath):
 			print(f" Event: {event}")
 			with open(file, "r") as read_file:
 				data = json.load(read_file, encoding="UTF-8")[event]['photometry']
-				print(data)
 			vegacount = 0
 			abcount = 0
 			othercount = 0
 			for dictionary in data:
-				print("dictionary found")
 				print(dictionary)
 				for key in dictionary:
-					print("key found")
-					print(key)
 					if key == "system":
-						print("system key found")
-						print(dictionary[key])
 						if dictionary[key] == "Vega":
-							#print(
 							vegacount += 1
-							print(vegacount)
+							print(f"Vega count: {vegacount}")
 						elif dictionary[key] == "AB":
 							abcount += 1
-							print(abcount)
+							print(f"AB count: {abcount}")
 						elif dictionary[key] != "Vega" and dictionary[key] != "AB":
 							othercount += 1
 							print("System not Ab/Vega, system name:")
 							print(dictionary[key])
-					else:
-						print("Incorrect key")
+					else: #make into elif for no system key
+						print("")
 	print(f"Data end. Vegacount: {vegacount}. AB count: {abcount}. Other count: {othercount}.")
 
-fxnname(filepath)
+systemCounts(filepath)
+systemCounts(filepath2)
 
