62e517eb110df23ef80499a6871e283befdebb62
moncluse <moncluse24@gmail.com>
2022-11-01 19:39:25 -0400

    attempt to include files with no photometry key

--- dataOrganized.py
+++ dataOrganized.py
@@ -1,4 +1,4 @@
-#working on cleaning up print statements
+#need to load file differently to do if statement for photometry separately
 import glob
 import json
 filepath = glob.glob("/home/emily.monclus/ArchiveData/kne-2000-2029/*")
@@ -15,28 +15,32 @@ def systemCounts(filepath):
 			event = last[0:-5]
 			print(f" Event: {event}")
 			with open(file, "r") as read_file:
-				data = json.load(read_file, encoding="UTF-8")[event]['photometry']
-			vegacount = 0
-			abcount = 0
-			othercount = 0
-			for dictionary in data:
-				print(dictionary)
-				for key in dictionary:
-					if key == "system":
-						if dictionary[key] == "Vega":
-							vegacount += 1
-							print(f"Vega count: {vegacount}")
-						elif dictionary[key] == "AB":
-							abcount += 1
-							print(f"AB count: {abcount}")
-						elif dictionary[key] != "Vega" and dictionary[key] != "AB":
-							othercount += 1
-							print("System not Ab/Vega, system name:")
-							print(dictionary[key])
-					else: #make into elif for no system key
-						print("")
-	print(f"Data end. Vegacount: {vegacount}. AB count: {abcount}. Other count: {othercount}.")
+				data = json.load(read_file, encoding="UTF-8")
+			eventdata = data[event]
+			keys = eventdata.keys()
+			if "photometry" not in keys:
+				print("No photometry")
+			else:
+				vegacount = 0
+				abcount = 0
+				othercount = 0
+				for dictionary in data:
+					print(dictionary)
+					for key in dictionary:
+						if key == "system":
+							if dictionary[key] == "Vega":
+								vegacount += 1
+								print(f"Vega count: {vegacount}")
+							elif dictionary[key] == "AB":
+								abcount += 1
+								print(f"AB count: {abcount}")
+							else: # dictionary[key] != "Vega" and dictionary[key] != "AB":
+								othercount += 1
+								print("System not Ab/Vega, system name:")
+								print(dictionary[key])
+							
+print(f"Data end. Vegacount: {vegacount}. AB count: {abcount}. Other count: {othercount}.")
 
 systemCounts(filepath)
-systemCounts(filepath2)
+#systemCounts(filepath2)
 
