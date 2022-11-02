0e75ba32bbed55ec7db3b7ec6757fdd43d0558ae
moncluse <moncluse24@gmail.com>
2022-11-02 15:57:04 -0400

    fixed counting for filepath1

--- dataOrganized.py
+++ dataOrganized.py
@@ -1,8 +1,8 @@
 #need to load file differently to do if statement for photometry separately
 import glob
 import json
-filepath = glob.glob("/home/emily.monclus/ArchiveData/kne-2000-2029/*")
-filepath2 = glob.glob("/home/emily.monclus/ArchiveData/kne-internal/*")
+filepath1 = glob.glob("/home/emily.monclus/kne-archival-pe/kne-2000-2029/*")
+filepath2 = glob.glob("/home/emily.monclus/kne-archival-pe/kne-internal/*")
 
 def systemCounts(filepath):
 	for file in filepath:
@@ -24,23 +24,28 @@ def systemCounts(filepath):
 				vegacount = 0
 				abcount = 0
 				othercount = 0
-				for dictionary in data:
+				for dictionary in data[event]['photometry']:
 					print(dictionary)
 					for key in dictionary:
 						if key == "system":
 							if dictionary[key] == "Vega":
+								print("Vega")
 								vegacount += 1
 								print(f"Vega count: {vegacount}")
 							elif dictionary[key] == "AB":
+								print("AB")
 								abcount += 1
 								print(f"AB count: {abcount}")
 							else: # dictionary[key] != "Vega" and dictionary[key] != "AB":
+								print("Other")
 								othercount += 1
 								print("System not Ab/Vega, system name:")
 								print(dictionary[key])
+						else:
+							print("not system")
 							
-print(f"Data end. Vegacount: {vegacount}. AB count: {abcount}. Other count: {othercount}.")
+			print(f"Data end. Vegacount: {vegacount}. AB count: {abcount}. Other count: {othercount}.")
 
-systemCounts(filepath)
+systemCounts(filepath1)
 #systemCounts(filepath2)
 
